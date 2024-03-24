997ae4ad1e5ef158fde5eaffaa1a59b4c39aadb2
sm791600 <stojanovikmarko7916@gmail.com>
2024-03-24 18:32:43 +0100

    zadacata raboti

--- src/main/java/vezbiSwitchCalculator/SwitchCalc.java
+++ src/main/java/vezbiSwitchCalculator/SwitchCalc.java
@@ -2,13 +2,43 @@
 операции: собирање, одземање, множење и делење. Во програмата треба да се
 напише метод со кој ќе се внесат двата операнди и соодветниот оператор. Доколку се
 внесе невалиден оператор соодветно да се постапи.*/
-package vezbiSwitchCalculator;
 
+package vezbiSwitchCalculator;
+import java.util.Scanner;
 public class SwitchCalc {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner vnes= new Scanner(System.in);
+		Scanner vnes2 = new Scanner(System.in);
+		System.out.println("Vnesi prv broj");
+		double a=vnes.nextDouble();
+		System.out.println("Vnesi znak za operacija");
+		String operacija=vnes2.nextLine();
+		System.out.println("Vnesi vtor broj");
+		double b=vnes.nextDouble();
+		double c;
+		switch(operacija) {
+		case "+":
+			c=a+b;
+			System.out.println(a+operacija+b+"="+c);
+			break;
+		case "-":
+			c=a-b;
+			System.out.println(a+operacija+b+"="+c);
+			break;
+		case "*":
+			c=a*b;
+			System.out.println(a+operacija+b+"="+c);
+			break;
+		case "/":
+			c=a/b;
+			System.out.println(a+operacija+b+"="+c);
+			break;
+			default:
+				System.out.println("Greska");
+				break;
+		}
 	}
 
 }
